8fce8ae70640ee82e7859e38311a870f14060fa9
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2020-11-06 09:53:45 -0700

    Init commit

---
+++ linked-list/linked-list.js
@@ -0,0 +1,60 @@
+class LinkedListNode {
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+  insert(num) {
+    let newNode = new LinkedListNode(num);
+    if(!this.head){
+      this.head = newNode;
+    } else {
+      let node = this.head;
+      while(node.next){
+        node = node.next;
+      }
+      node.next = newNode;
+    }
+  }
+
+  remove(num) {
+    let node = this.head;
+    let previous = null;
+    while(node){
+      if(node.data == num){
+        if(previous != null){
+          previous.next = node.next;
+        } else {
+          this.head = null;
+        }
+        return;
+      }
+      previous = node;
+      node = node.next;
+    }
+  }
+
+  find(num){
+    let node = this.head;
+    while(node){
+      if(node.data == num)return true;
+      node = node.next;
+    }
+    return false;
+  }
+
+  size(){
+    let node = this.head;
+    let count = 0;
+    while(node){
+      count ++;
+      node = node.next;
+    }
+    return count;
+  }
+}
